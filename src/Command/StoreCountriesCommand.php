261f32dfc577dbf7b1d39c437519d37036ed2057
Tedd <tedd@Edouards-Air.broadband>
2020-04-07 21:35:51 +0100

    formats file content, news up entity and insert into DB

--- src/Command/StoreCountriesCommand.php
+++ src/Command/StoreCountriesCommand.php
@@ -8,17 +8,26 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Country;
 
 class StoreCountriesCommand extends Command
 {
     protected static $defaultName = 'store-countries';
+
+    public $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        parent::__construct();
+        $this->em = $em;
+    }
 
     protected function configure()
     {
         $this
             ->setDescription('Add a short description for your command')
             ->addArgument('filepath', InputArgument::OPTIONAL, 'path to the file containing the countries')
-            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
         ;
     }
 
@@ -28,15 +37,46 @@ class StoreCountriesCommand extends Command
         $filepath = $input->getArgument('filepath');
 
         if ($filepath) {
-            $content = file_get_contents($filepath);
-            $io->note(sprintf('You passed an argument: %s', $filepath));
+            //turn the content of the file into an array
+            $countries = file($filepath);
+            
+            foreach ($countries as $country) {
+                //avoid short, irrelevant lines (first and last one)
+                if (strlen($country) < 5) {
+                    continue;
+                }
+
+                //Time for a little cleaning. Start by removing whitespaces on each side
+                $country = trim($country);
+
+                //remove that pesky comma on the right
+                $country = rtrim($country, ',');
+
+                //json loves double quotes, replace single with doubles
+                $country = str_replace('\'', '"', $country);
+
+                //In order to add double quotes to our future keys (name and code), we
+                //search for them and replace them accordingly.
+                $country = str_replace('code:', '"code":', $country);
+                $country = str_replace('name:', '"name":', $country);
+                
+                //turning the string into an object, for inserting into entity
+                $country = json_decode($country);
+                
+                //after creating new entity, insert data
+                $newCountry = new Country();
+                $newCountry->setName($country->name);
+                $newCountry->setCode($country->code);
+                
+                //insert data into database
+                $this->em->persist($newCountry);
+                $this->em->flush();
+            }
+
+            $io->note(sprintf('You have succesfully inserted new countries into the DB'));
         }
 
-        if ($input->getOption('option1')) {
-            // ...
-        }
-
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+        $io->success('Success ! Please browse to /country in order to view results');
 
         return 0;
     }
